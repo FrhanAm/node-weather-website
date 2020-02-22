019b63475482045abb4999cced69166d5b7aa003
Farhan M <farhan9aqaei@gmail.com>
2020-02-22 16:36:19 +0330

    add more information for forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const forecast = (longitude, latitude, callback) => {
-    const url = "https://api.darksky.net/forecast/3ddd961e72f01a216cfd592fb1f820ea/" + longitude + "," + latitude ;
+    const url = "https://api.darksky.net/forecast/3ddd961e72f01a216cfd592fb1f820ea/" + longitude + "," + latitude + "?units=si" ;
 
     request({url, json: true}, (error, {body}) => {
         if(error) {
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if(body.error) {
             callback("Unable to find location!", undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is " + body.currently.precipProbability + " chance of rain.");
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is " + body.currently.precipProbability + " chance of rain. and the lowest temperature is " + body.daily.data[0].temperatureLow + " with a highest temperature of " + body.daily.data[0].temperatureHigh);
         }
     });
 }
